1a7bba5c9d90f132e2788d8decf5e08adee8f9d6
Jamie Bliss <astronouth7303@gmail.com>
2015-10-24 16:13:30 -0400

    Require permissions to see reports

--- reports/views.py
+++ reports/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .reports import Report
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index_view(request):
     # List possible reports
     return render(request, 'reports/index.html', {
         'reports': sorted(Report.iterreports(), key=lambda r: r.name),
     })
 
+@login_required
 def report_view(request, slug):
     # Find and instantiate an individual report
     rprt = Report.getreport(slug)(request.GET)
