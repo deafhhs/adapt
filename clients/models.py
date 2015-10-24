12c336dd36b85e3b3da9f53a50b00bc96b626d81
Jamie Bliss <astronouth7303@gmail.com>
2015-10-23 22:27:26 -0400

    Spaces!

--- clients/models.py
+++ clients/models.py
@@ -2,33 +2,40 @@ from django.db import models
 import localflavor.us.models as lfmodels
 
 GENDER_CHOICES = [
-	('M', 'Male'),
-	('F', 'Female'),
-	('O', 'Other'),
+    ('M', 'Male'),
+    ('F', 'Female'),
+    ('O', 'Other'),
+]
+
+RACE_CHOICES = [
+    
 ]
 
 class Client(models.Model):
-	first_name = models.CharField(max_length=64)
-	last_name = models.CharField(max_length=64)
-	gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-	date_of_birth = models.DateField()
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    date_of_birth = models.DateField()
 
-	intake_date = models.DateField(auto_now_add=True)
+    intake_date = models.DateField(auto_now_add=True)
 
-	address = models.TextField(null=True, blank=True)
-	city = models.CharField(null=True, blank=True, max_length=64)
-	state = lfmodels.USPostalCodeField(null=True, blank=True, default='MI')
-	zip_code = lfmodels.USZipCodeField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    city = models.CharField(null=True, blank=True, max_length=64)
+    state = lfmodels.USPostalCodeField(null=True, blank=True, default='MI')
+    zip_code = lfmodels.USZipCodeField(null=True, blank=True)
 
-	email = models.EmailField(null=True, blank=True)
-	phone = lfmodels.PhoneNumberField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    phone = lfmodels.PhoneNumberField(null=True, blank=True)
 
-	spouse = models.CharField(null=True, blank=True, max_length=128)
+    spouse = models.CharField(null=True, blank=True, max_length=128)
 
-	is_veteran = models.BooleanField(default=True)
-	live_alone = models.BooleanField()
-	family_size = models.PositiveSmallIntegerField(default=1)
+    is_veteran = models.BooleanField(default=True)
+    live_alone = models.BooleanField()
+    family_size = models.PositiveSmallIntegerField(default=1)
 
-	emergency_contact = models.CharField(null=True, blank=True, max_length=128)
-	emergency_phone = lfmodels.PhoneNumberField(null=True, blank=True)
-	
+    emergency_contact = models.CharField(null=True, blank=True, max_length=128)
+    emergency_phone = lfmodels.PhoneNumberField(null=True, blank=True)
+
+    race = models.CharField(choices=RACE_CHOICES, max_length=32)
+    is_hispanic = models.BooleanField()
+    additional_races = models.CharField(max_length=256)
