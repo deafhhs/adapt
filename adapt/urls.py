892888f7f619cb403d83c02e50d88648263110b0
Jamie Bliss <astronouth7303@gmail.com>
2015-10-24 22:54:15 -0400

    Wrong urls file

--- adapt/urls.py
+++ adapt/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 import reports.urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
+    url(r'^admin/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'^reports/', include(reports.urls)),
     url(r'^$', include(admin.site.urls)),
 ]
--- reports/urls.py
+++ reports/urls.py
@@ -1,13 +1,8 @@
 from django.conf.urls import url
 
 from . import views
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.index_view, name='index'),
     url(r'^(?P<slug>[^/]+)/$', views.report_view, name='report'),
-    url(r'^admin/password_reset/$', auth_views.password_reset, name='admin_password_reset'),
-    url(r'^admin/password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
-]
+ ]
