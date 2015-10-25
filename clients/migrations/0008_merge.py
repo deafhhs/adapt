5f4cc1e5ec018203ae1d87f47c51a99b9724ee0b
Dave Brondsema <dave@brondsema.net>
2015-10-24 21:21:48 -0400

    merge migrations

---
+++ clients/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('clients', '0007_auto_20151024_2056'),
+        ('clients', '0007_settings'),
+    ]
+
+    operations = [
+    ]
